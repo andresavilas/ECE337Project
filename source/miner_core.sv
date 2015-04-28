7787dd0775c0114ee649fa0212d80961999f4154
Mentor Graphics <mg98@ecegrid-thin1.ecn.purdue.edu>
2015-04-28 11:47:37 -0400

    Changed inputs to proper core inputs, core is fully functional

--- source/miner_core.sv
+++ source/miner_core.sv
@@ -7,27 +7,32 @@
 // Description: Miner Core Top Level
 module miner_core(
   input wire clk, n_rst, hash_enable,
-  input wire [0:639] message,
+  input wire [0:607] block,
+  input wire [0:31] nonce,
+  input wire [0:255] target,
   output wire finished,
-  //output wire correct,
-  output wire [0:255] hash
+  output wire correct,
+  output wire [0:255] hashed
 );
 reg msa_en, comp_en, add_en, msa2_en, comp2_en, add2_en, rollover_flag, select, enable_timer;
 reg [6:0]count;
 reg [6:0]rollover_val;
-reg [0:511]chunk1;
+wire [0:639]message;
+reg [0:511]chunk1;//For the first and second chunk of the First round of SHA
 reg [0:511]chunk2;//Second round of SHA
 reg [0:7] [0:31]fh;
 reg [0:255]h;
 reg [0:255]temphash;
+assign message = {block,nonce};
 assign fh = (select == 1) ? h : {32'h6a09e667, 32'hbb67ae85, 32'h3c6ef372, 32'ha54ff53a, 32'h510e527f, 32'h9b05688c, 32'h1f83d9ab, 32'h5be0cd19};
 assign chunk1 = (select == 1) ? {message[512:639], 1'b1, 319'b0, 64'b0000000000000000000000000000000000000000000000000000001010000000} : message[0:511];
 assign chunk2 = {h,1'b1,191'b0,64'b0000000000000000000000000000000000000000000000000000000100000000};
+assign correct = ((hashed < target) && finished) ? 1 : 0;
 genvar i;
 generate 
   for(i=0; i < 249; i=i+8)
   begin
-    assign hash[i:i+7] = temphash[248-i:255-i];
+    assign hashed[i:i+7] = temphash[248-i:255-i];
   end
 endgenerate
 miner_core_CCU CCU1(.clk(clk), .n_rst(n_rst), .hash_enable(hash_enable), .rollover_flag(rollover_flag), .select(select), .enable_timer(enable_timer), .rollover_val(rollover_val), .msa_en(msa_en), .comp_en(comp_en), .msa2_en(msa2_en), .comp2_en(comp2_en), .add_en(add_en), .add2_en(add2_en), .finished(finished));
--- source/tb_miner_core.sv
+++ source/tb_miner_core.sv
@@ -8,9 +8,11 @@
 `timescale 1ns / 10ps
 localparam CLK_PERIOD = 20;
 module tb_miner_core();
-  reg clk, n_rst, hash_enable, finished, hashed;
-  reg [0:639]message;
-  reg [0:255]hash;
+  reg clk, n_rst, hash_enable, finished, correct;
+  reg [0:607]block;
+  reg [0:31]nonce;
+  reg [0:255]hashed;
+  reg [0:255]target;
   always
 	 begin
 	 clk = 1'b0;
@@ -18,12 +20,14 @@ module tb_miner_core();
 	 clk = 1'b1;
 	 #(CLK_PERIOD/2.0);	  
 	end
-	miner_core CORE1 (.clk(clk), .n_rst(n_rst), .hash_enable(hash_enable), .message(message), .finished(finished), .hash(hash));
+	miner_core CORE1 (.clk(clk), .n_rst(n_rst), .hash_enable(hash_enable), .block(block), .nonce(nonce), .target(target), .finished(finished), .correct(correct), .hashed(hashed));
 	
 	initial
 	begin 
 	 n_rst = 0;
-	 message = 640'h0100000081cd02ab7e569e8bcd9317e2fe99f2de44d49ab2b8851ba4a308000000000000e320b6c2fffc8d750423db8b1eb942ae710e951ed797f7affc8892b0f1fc122bc7f5d74df2b9441a42a14695;
+	 block = 608'h0100000081cd02ab7e569e8bcd9317e2fe99f2de44d49ab2b8851ba4a308000000000000e320b6c2fffc8d750423db8b1eb942ae710e951ed797f7affc8892b0f1fc122bc7f5d74df2b9441a;
+	 nonce = 32'h42a14695;
+	 target = 256'h00000000000444b9f20000000000000000000000000000000000000000000000;
 	 //message appended with nonce 42a14695
   	#(2*CLK_PERIOD);
 	 @(negedge clk);
